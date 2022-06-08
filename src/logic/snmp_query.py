1765f204da348d196388feef383b9788a786c7e9
Jeremy Zemasch <jeremy.zemasch@getzner.com>
2022-06-08 13:38:49 +0200

    Added MODULE_LOGGER_HEAD to logger.debug to know which file threw the error.

--- src/logic/snmp_query.py
+++ src/logic/snmp_query.py
@@ -23,27 +23,8 @@ MODULE_LOGGER_HEAD = "snmp_query -> "
 #                      functions
 # ------------------------------------------------------- #
 def snmp_input_state(host, community):
-    logger.debug("Entered snmp_input_state in snmp_qery")
+    logger.debug(MODULE_LOGGER_HEAD + "Entered snmp_input_state in snmp_query")
     oid = '1.3.6.1.2.1.33.1.3.3.1.3.1'
-    error_indication, error_status, error_index, var_snmp= cmdgen.CommandGenerator().getCmd(
-        cmdgen.CommunityData(community),
-        cmdgen.UdpTransportTarget((host, 161)),
-        cmdgen.MibVariable(oid),
-        lookupMib=False,
-    )
-    if error_indication:
-        logger.debug(MODULE_LOGGER_HEAD + "Entered Error indication. Val set to 1: {}".format(var_snmp))
-        logger.debug(MODULE_LOGGER_HEAD + "Sending 1 Value.")
-        logger.debug(MODULE_LOGGER_HEAD + "IP: {}".format(host))
-        return int(1)
-    for oid, val in var_snmp:
-        return_value = int(val)
-        return return_value
-
-
-def snmp_hostname(host, community):
-    logger.debug(MODULE_LOGGER_HEAD + "Entered smp_hostname in snmp_query")
-    oid = '1.3.6.1.2.1.1.5.0'
     error_indication, error_status, error_index, var_snmp = cmdgen.CommandGenerator().getCmd(
         cmdgen.CommunityData(community),
         cmdgen.UdpTransportTarget((host, 161)),
@@ -51,11 +32,14 @@ def snmp_hostname(host, community):
         lookupMib=False,
     )
     if error_indication:
-        logger.debug(MODULE_LOGGER_HEAD + "Entered error indication in snmp_hostname. Returning Value XXATBU-UNKNOWN")
-        return str("XXATBU-UNKNOWN")
+        logger.debug(MODULE_LOGGER_HEAD + "Entered Error indication. Val set to 1")
+        logger.debug(MODULE_LOGGER_HEAD + "Returning Value for IP: {}".format(host))
+        return int(1)
     for oid, val in var_snmp:
         return_value = int(val)
         return return_value
+
+
 # ------------------------------------------------------- #
 #                      classes
 # ------------------------------------------------------- #
@@ -65,4 +49,4 @@ def snmp_hostname(host, community):
 #                       main
 # ------------------------------------------------------- #
 if __name__ == "__main__":
-    print(snmp_input_state("10.11.41.66", "gwsread"))
+    print(snmp_input_state("111.111.111.111", "public"))
