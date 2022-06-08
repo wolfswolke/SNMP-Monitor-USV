13ee67cc930279787a679ddc768c0c762bf5e6d6
Jeremy Zemasch <jeremy.zemasch@getzner.com>
2022-06-08 13:39:04 +0200

    Rebuilt the File to not send multiple alerts if one USV is offline.

--- src/start_app.py
+++ src/start_app.py
@@ -7,7 +7,7 @@
 
 from gutils.config_handle import config
 from gutils.logging_handle import logger
-from logic.snmp_query import snmp_input_state, snmp_hostname
+from logic.snmp_query import snmp_input_state
 from logic.discord_webhook import alert as discord_alert
 
 from time import sleep
@@ -15,7 +15,7 @@ from time import sleep
 # ------------------------------------------------------- #
 #                   definitions
 # ------------------------------------------------------- #
-APP_VERSION = "v1.0.0"
+APP_VERSION = "v1.0.4"
 MODULE_LOGGER_HEAD = "start_app -> "
 
 
@@ -30,6 +30,7 @@ MODULE_LOGGER_HEAD = "start_app -> "
 def setup_logging(level):
     logger.set_logging_level(level)
     logger.set_cmd_line_logging_output()
+
 
 # ------------------------------------------------------- #
 #                      classes
@@ -50,19 +51,31 @@ if __name__ == "__main__":
         logger.info("            SNMP Monitor Started {}".format(APP_VERSION))
         logger.info("-----------------------------------------------------------")
 
-        clients = dict(config.get_element("clients")).items()
+        clients = config.get_element("clients")
         snmp_password = config.get_element("general", "snmp_password")
         timeout = config.get_element("general", "check_cycle_in_minutes")
 
         while True:
-            for client in clients:
-                ip, hostname = client
+            for dict_key, dict_value in clients.items():
+                ip = str(dict_key)
+                hostname = str(dict_value["hostname"])
                 snmp_result = snmp_input_state(ip, snmp_password)
                 logger.debug(MODULE_LOGGER_HEAD + "Voltage: " f"{snmp_result}")
+
                 if snmp_result < 200:
-                    discord_alert(hostname)
+                    if bool(dict_value["offline"]) is False:
+                        logger.debug(MODULE_LOGGER_HEAD + "Sending Alert and marking Device as Offline.")
+                        discord_alert(hostname)
+                        dict_value["offline"] = True
+                    else:
+                        logger.debug(MODULE_LOGGER_HEAD + "Device Still offline")
+
+                else:
+                    dict_value["offline"] = False
+                    logger.debug(MODULE_LOGGER_HEAD + "Is back online!")
+
             logger.info("Finished Check. Going to Sleep...")
-            sleep(timeout*60)
+            sleep(timeout * 60)
 
     except KeyboardInterrupt:
         logger.info("-----------------------------------------------------------")
